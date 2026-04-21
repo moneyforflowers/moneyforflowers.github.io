30b48fa1b25b649bbc9e487702b1eb81b89bbea7
Hookibibi <115881751+Oukibibi@users.noreply.github.com>
2026-04-21 19:03:55 +0200

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Home() {
-    useEffect(() => {
-        var result = axios.post('https://id.twitch.tv/oauth2/token?client_id=magcfm9155x67mc5tcfud1larpcmnd&client_secret=8v5mxvrvryg16ac6b109nzrx7zv7dt&grant_type=client_credentials')
-        .then(response => console.log(response.data))
-    }, [])
+    // useEffect(() => {
+    //     var result = axios.post('https://id.twitch.tv/oauth2/token?client_id=magcfm9155x67mc5tcfud1larpcmnd&client_secret=8v5mxvrvryg16ac6b109nzrx7zv7dt&grant_type=client_credentials')
+    //     .then(response => console.log(response.data))
+    // }, [])
     return <>Home</>;
 }
 
