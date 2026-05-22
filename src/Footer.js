4d5c3cf6e97f4baef9c678f43f88c76824751743
Hookibibi <115881751+Oukibibi@users.noreply.github.com>
2026-05-22 19:03:58 +0200

    remove twitter

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,6 @@ function Footer() {
         <div className="reseauxDiv">
             <p>Nos réseaux</p>
             <div className="reseaux">
-                <a href="https://twitter.com/MoneyForFlowers" target="_blank" rel="noreferrer"><img src="twitterWhite.png" alt="Twitter"/></a>
                 <a href="https://bsky.app/profile/moneyforflowers.bsky.social" target="_blank" rel="noreferrer"><img src="blueskyWhite.png" alt="Bluesky"/></a>
             </div>
         </div>
